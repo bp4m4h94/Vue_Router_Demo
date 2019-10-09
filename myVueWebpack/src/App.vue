bac254c447671e0dce1c95d5392d0851f14d7335
Ryan <bp4m4h94@gmail.com>
2019-10-09 13:40:53 +0800

    Revert "feat : 自訂一切換路由"
    This reverts commit 8f2a4b8d8c258094d178998aa293553b911c674e.

--- myVueWebpack/src/App.vue
+++ myVueWebpack/src/App.vue
@@ -10,15 +10,6 @@
       <li class="nav-item">
         <routerLink class="nav-link" to="/page1">Page1</routerLink>
       </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link" @click="goPage2">Page2</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link" @click="goLastPage">回前頁</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link" @click="goNextPage">回前頁</a>
-      </li>
     </ul>
   </div>
 </nav>
@@ -39,19 +30,7 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  },
-  methods: {
-    goPage2(){
-      this.$router.push('/page1');
-    },
-    goLastPage(){
-      this.$router.back()
-      console.log(this.$router);
-    },
-    goNextPage(){
-      this.$router.go()
-    }
-  },
+  }
 }
 </script>
 
